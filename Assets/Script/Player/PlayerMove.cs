dad687d07ec952a70d5ffc349deb1f28bf1708b5
MurasakiIzumi <dova879537478@gmail.com>
2025-05-20 23:16:49 +0900

    0520_playermove0.1

--- Assets/Script/Player/PlayerMove.cs
+++ Assets/Script/Player/PlayerMove.cs
@@ -3,23 +3,65 @@ using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float speed;
+    public float movespeed;
+    public float rotatespeed;
     private Rigidbody rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb=GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+    {
+        PlayerMoveNormal();
+        PlayerMoveDash();
+        PlayerRotate();
+    }
+
+    private void PlayerMoveNormal()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertiacl = Input.GetAxis("Vertical");
 
-        Vector3 force = new Vector3(horizontal * speed, 0.0f, vertiacl * speed);
+        Vector3 move_velocity = new Vector3(horizontal, 0.0f, vertiacl);
 
-        rb.AddForce(force);
+        move_velocity = transform.TransformDirection(move_velocity);
+
+        transform.position += move_velocity * movespeed * Time.deltaTime;
+    }
+
+    private void PlayerMoveDash()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertiacl = Input.GetAxis("Vertical");
+
+            Vector3 move_velocity = new Vector3(horizontal, 0.0f, vertiacl);
+
+            move_velocity = transform.TransformDirection(move_velocity);
+
+            transform.position += move_velocity * movespeed * 2f * Time.deltaTime;
+        }
+
+    }
+
+    private void PlayerRotate()
+    {
+        int rotate_direction = 0;
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rotate_direction = -1;
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            rotate_direction = 1;
+        }
+
+        transform.Rotate(0, rotate_direction * rotatespeed, 0);
     }
 }
