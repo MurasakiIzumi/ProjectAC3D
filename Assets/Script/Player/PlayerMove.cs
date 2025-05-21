2f9440b1a761d4a4bf79142ab27f5e466850a66f
MurasakiIzumi <dova879537478@gmail.com>
2025-05-21 17:26:38 +0900

    0521_playermove0.5

--- Assets/Script/Player/PlayerMove.cs
+++ Assets/Script/Player/PlayerMove.cs
@@ -1,63 +1,87 @@
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float movespeed;
+    public float MaxSpeed;
     public float rotatespeed;
-    private Rigidbody rb;
+    [SerializeField] private int gear;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float targetSpeed;
+    [SerializeField] private float upSpeed;
+    [SerializeField] private float downSpeed;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        gear = 0;
+        moveSpeed = 0;
+        targetSpeed = 0;
+        upSpeed = MaxSpeed / 5f;
+        downSpeed = MaxSpeed / 2f;
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        PlayerMoveNormal();
-        PlayerMoveDash();
-        PlayerRotate();
+        ChangeGear();
+        ChangeTargetSpeed();
+        ChangeSpeed();
+        Move();
+        Rotate();
     }
 
-    private void PlayerMoveNormal()
+    private void ChangeGear()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertiacl = Input.GetAxis("Vertical");
-
-        Vector3 move_velocity = new Vector3(horizontal, 0.0f, vertiacl);
-
-        move_velocity = transform.TransformDirection(move_velocity);
-
-        transform.position += move_velocity * movespeed * Time.deltaTime;
-    }
-
-    private void PlayerMoveDash()
-    {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            float horizontal = Input.GetAxis("Horizontal");
-            float vertiacl = Input.GetAxis("Vertical");
-
-            Vector3 move_velocity = new Vector3(horizontal, 0.0f, vertiacl);
-
-            move_velocity = transform.TransformDirection(move_velocity);
-
-            transform.position += move_velocity * movespeed * 2f * Time.deltaTime;
+            gear++;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            gear--;
         }
 
+        gear = Mathf.Max(-1, Mathf.Min(gear, 5));
     }
 
-    private void PlayerRotate()
+    private void ChangeTargetSpeed()
+    {
+        targetSpeed = gear * MaxSpeed / 5f;
+    }
+
+    private void ChangeSpeed()
+    {
+        if (targetSpeed > moveSpeed)
+        {
+            moveSpeed += upSpeed * Time.deltaTime;
+        }
+        else if (targetSpeed < moveSpeed)
+        {
+            moveSpeed -= downSpeed * Time.deltaTime;
+        }
+
+        if (Mathf.Abs(targetSpeed - moveSpeed) <= 0.1f)
+        {
+            moveSpeed = targetSpeed;
+
+        }
+    }
+
+    private void Move()
+    {
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+    }
+
+    private void Rotate()
     {
         int rotate_direction = 0;
 
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKey(KeyCode.A))
         {
             rotate_direction = -1;
         }
-        else if (Input.GetKey(KeyCode.E))
+        else if (Input.GetKey(KeyCode.D))
         {
             rotate_direction = 1;
         }
